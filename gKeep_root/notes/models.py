57e95c3821b9872662e4708e9ccb8793c7bc5acb
Shivaji Thattikota <shivajithattikota@outlook.com>
2020-07-21 22:51:36 +0530

    Added functionality to view apps dynamically

--- gKeep_root/notes/models.py
+++ gKeep_root/notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    data = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
--- gKeep_root/notes/views.py
+++ gKeep_root/notes/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .models import Todo
 # Create your views here.
+
 def index(request):
-    context = {}
+    todos = Todo.objects.all()
+    # form = TodoForm(request.Post)
+
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        todo_object = Todo(data=content)
+        todo_object.save()
+
+    
+    context = {'todos':todos}
     return render(request, 'notes/index.html', context)
