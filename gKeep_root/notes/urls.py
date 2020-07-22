93014781df8c2e6b9d462536743f6c24b6b28cba
Shivaji Thattikota <shivajithattikota@outlook.com>
2020-07-22 10:51:03 +0530

    Created delete functionality

--- gKeep_root/notes/urls.py
+++ gKeep_root/notes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('delete_todo/<str:pk>/', views.deleteTodo, name='delete_todo')
 ]
--- gKeep_root/notes/views.py
+++ gKeep_root/notes/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import redirect, render
+
+from .forms import TodoForm
 from .models import Todo
 # Create your views here.
 
 def index(request):
     todos = Todo.objects.all()
+    form = TodoForm
     # form = TodoForm(request.Post)
 
     if request.method == 'POST':
@@ -11,6 +14,11 @@ def index(request):
         todo_object = Todo(data=content)
         todo_object.save()
 
-    
     context = {'todos':todos}
     return render(request, 'notes/index.html', context)
+
+
+def deleteTodo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    todo.delete()
+    return redirect('/')    
